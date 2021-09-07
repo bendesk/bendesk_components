393e3628c96eaae28a3be41865b1c8d74902cf9e
Ajith Paul <ajith@bendesk.de>
2021-09-07 17:30:17 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
 import { TypographyTeaser } from './TypographyTeaser'
-const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+import { ColoredLineCard } from './ColoredLineCard'
 
-export { TypographyTeaser, ExampleComponent }
+export { TypographyTeaser, ColoredLineCard }
