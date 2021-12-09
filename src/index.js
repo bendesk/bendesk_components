b0740717bce40c8994db2c9616b24ad9d1ad151d
Armin Ayatollahi <arminayat3@gmail.com>
2021-12-09 17:02:03 +0330

    feat(index.js): export Chip component ++ refactor imports and exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import styles from './styles.module.css'
-import { TypographyTeaser } from './TypographyTeaser'
-import { ColoredLineCard } from './ColoredLineCard'
-import { InfoWithIcon } from './InfoWithIcon'
+// Atoms
 import { Typography } from './Atoms/Typography'
 import Colors from './Atoms/Colors'
+// Molecules
+import { Chip } from './molecules/Chip'
+// Organisms
+import { TypographyTeaser } from './organisms/cards/TypographyTeaser'
+import { ColoredLineCard } from './organisms/cards/ColoredLineCard'
+import { InfoWithIcon } from './organisms/infoContainer/InfoWithIcon'
 
-export { Typography, Colors, TypographyTeaser, ColoredLineCard, InfoWithIcon }
+export {
+  //Atoms
+  Typography,
+  Colors,
+  //Molecules
+  Chips,
+  //Organisms
+  TypographyTeaser,
+  ColoredLineCard,
+  InfoWithIcon
+}
