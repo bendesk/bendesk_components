8f046743b9985ed70ad479d01f5e15545a9aab94
Armin Ayatollahi <arminayat3@gmail.com>
2021-12-23 11:37:15 +0330

    fix(Checkbox): update margin on Horizonntal mode

--- src/molecules/Checkbox/styles.ts
+++ src/molecules/Checkbox/styles.ts
@@ -2,6 +2,6 @@ import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
   rootV: { alignItems: 'start !important' },
-  rootH: { alignItems: 'start !important', marginRight: '28px' },
+  rootH: { alignItems: 'start !important', marginRight: '40px' },
   label: { marginTop: '12px' }
 }))
