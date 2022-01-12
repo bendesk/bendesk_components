033484893e02ea1112cdb6ed6fca9b82a5c1040c
Armin Ayatollahi <arminayat3@gmail.com>
2022-01-12 12:52:19 +0330

    feat(Icons): add ArrowDownOutlined Icon

---
+++ src/static/icons/bendesk/ArrowDownOutlined.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { SvgIcon } from '@material-ui/core'
+
+export default ({ style }: { style?: React.CSSProperties }) => (
+  <SvgIcon
+    width='35px'
+    height='35px'
+    viewBox='0 0 35 35'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    style={style}
+  >
+    <g clipPath='url(#clip0_9617_3993)'>
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M17.5 -1.5299e-06C7.83502 -2.37484e-06 2.37484e-06 7.83501 1.5299e-06 17.5C6.84959e-07 27.165 7.83502 35 17.5 35C27.165 35 35 27.165 35 17.5C35 7.83502 27.165 -6.84959e-07 17.5 -1.5299e-06ZM16.5888 10.695L18.6091 10.695L18.6091 20.8709L22.453 17.0134L23.8631 18.41L17.5989 24.6606L11.3483 18.41L12.7313 17.0134L16.5888 20.8709L16.5888 10.695Z'
+        fill='#126AEF'
+      />
+    </g>
+    <defs>
+      <clipPath id='clip0_9617_3993'>
+        <rect width='35' height='35' fill='white' />
+      </clipPath>
+    </defs>
+  </SvgIcon>
+)
