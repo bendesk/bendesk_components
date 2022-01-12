e82f28a2c30409633ecd4307a6e2032bb4b0684b
Armin Ayatollahi <arminayat3@gmail.com>
2022-01-12 16:53:29 +0330

    feat: update index file with new Components

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@
 import { Typography } from './atoms/Typography'
 import Colors from './atoms/Colors'
 // Molecules
+import { Accordion } from './molecules/Accordion'
+import { Avatar } from './molecules/Avatar'
+import { CheckboxGroup } from './molecules/Checkbox'
+import { CircularProgress, LinearProgress } from './molecules/Progress'
 import { Chip } from './molecules/Chip'
+import { Pagination } from './molecules/Pagination'
+import { RadiobuttonGroup } from './molecules/Radiobutton'
+import { Select } from './molecules/Select'
+import { Snackbar } from './molecules/Snackbar'
+import { Switch } from './molecules/Switch'
+import { TextField } from './molecules/TextField'
 // Organisms
 import { TypographyTeaser } from './organisms/cards/TypographyTeaser'
 import { ColoredLineCard } from './organisms/cards/ColoredLineCard'
@@ -13,7 +23,18 @@ export {
   Typography,
   Colors,
   // Molecules
+  Accordion,
+  Avatar,
+  CheckboxGroup,
+  CircularProgress,
   Chip,
+  LinearProgress,
+  Pagination,
+  RadiobuttonGroup,
+  Select,
+  Snackbar,
+  Switch,
+  TextField,
   // Organisms
   TypographyTeaser,
   ColoredLineCard,
