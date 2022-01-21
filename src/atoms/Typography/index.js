19995b5555a210743e4029b41aea5a903e3a09a2
Ajith Paul <ajith@bendesk.de>
2022-01-21 10:20:00 +0100

    fix: file casing issue

---
+++ src/atoms/Typography/index.js
@@ -0,0 +1,36 @@
+import MuiTypography from '@material-ui/core/Typography'
+import React from 'react'
+import PropTypes from 'prop-types'
+import colors from '../../shared/colors'
+
+export const Typography = ({
+  variant,
+  children,
+  color = 'primary',
+  ...props
+}) => {
+  return (
+    <MuiTypography
+      key={variant}
+      variant={variant}
+      style={{ marginBottom: '24px', color: colors[color] }}
+      {...props}
+    >
+      {children}
+    </MuiTypography>
+  )
+}
+
+Typography.propTypes = {
+  color: PropTypes.oneOf([
+    'textPrimary',
+    'success',
+    'warning',
+    'error',
+    'purple'
+  ])
+}
+
+Typography.defaultProps = {
+  color: 'textPrimary'
+}
---
+++ src/shared/colors.js
@@ -0,0 +1,8 @@
+export default {
+  blue: '#126AEF',
+  white: '#ffffff',
+  purple: '#8A85FF',
+  error: '#FF6848',
+  warning: '#FFAE11',
+  success: '#6CC83D'
+}
